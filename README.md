2ac841b058349487437a0e24d22d8ff91af7caa3
Andre Weissflog <floooh@gmail.com>
2020-01-10 16:20:17 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # fips-utils
 Grab-bag of useful fips verb- and generator-script not in "fips core"
+
+## Usage
+
+Just add the ```fips-utils``` import to the ```fips.yml``` file
+of your project and run ```fips fetch```.
+
+```yaml
+---
+imports:
+    fips-utils:
+        git: https://github.com/fips-libs/fips-utils
+```
+
+Now run ```fips help```, this should list additional verbs (subcommands),
+```fips list configs```, and check out the additional generators
+under ```fips-files/generators``` and cmake helper macros in
+```fips-files/include.cmake```.
---
+++ fips-files/generators/copy.py
@@ -0,0 +1,98 @@
+'''
+    Copy files from project directory into the deployment directory.
+
+    Usage:
+
+    Create a YAML file with a list of files to copy and (optional)
+    options:
+
+    ---
+    options:
+        src_dir: [optional relative source directory]
+        dst_dir: [optional relative destination directory]
+        # optional platform-specific overrides
+        ios:
+            dst_dir:    [optional relative dst_dir for iOS]
+        macos:
+            dst_dir:    [optional relative dst_dir for macOS]
+    files:
+        - DroidSansJapanese.ttf
+        - DroidSerif-Bold.ttf
+        - DroidSerif-Italic.ttf
+        - DroidSerif-Regular.ttf
+
+    By default the files are expected in the same directory as the .yml file,
+    and are copied to the root of the deploy dir (where the executable is
+    located).
+'''
+Version = 1
+
+import genutil as util
+import subprocess
+import shutil
+import yaml
+import os
+import platform
+
+#-------------------------------------------------------------------------------
+def copy_files(src_dir, dst_dir, yml):
+    for filename in yml['files']:
+        src = src_dir + filename
+        dst = dst_dir + filename
+        print("## cp '{}' => '{}'".format(filename, dst))
+        if not os.path.exists(os.path.dirname(dst)):
+            os.makedirs(os.path.dirname(dst))
+        try:
+            shutil.copyfile(src, dst)
+        except IOError as err:
+            # show a proper error if file copying fails
+            util.fmtError("Failed to copy file '{}' with '{}'".format(err.filename, err.strerror))
+
+
+#-------------------------------------------------------------------------------
+def gen_header(out_hdr, yml):
+    with open(out_hdr, 'w') as f:
+        f.write('#pragma once\n')
+        f.write('//------------------------------------------------------------------------------\n')
+        f.write('// #version:{}#\n'.format(Version))
+        f.write('// machine generated, do not edit!\n')
+        f.write('//------------------------------------------------------------------------------\n')
+        f.write('// JUST A DUMMY FILE, NOTHING TO SEE HERE\n')
+
+#-------------------------------------------------------------------------------
+def check_dirty(src_root_path, input, out_hdr, yml):
+    out_files = [out_hdr]
+    in_files  = [input]
+    for filename in yml['files']:
+        in_files.append(os.path.abspath(src_root_path + filename))
+    return util.isDirty(Version, in_files, out_files)
+
+#-------------------------------------------------------------------------------
+def generate(input, out_src, out_hdr, args):
+    with open(input, 'r') as f:
+        try:
+            yml = yaml.load(f)
+        except yaml.YAMLError as exc:
+            # show a proper error if YAML parsing fails
+            util.setErrorLocation(exc.problem_mark.name, exc.problem_mark.line-1)
+            util.fmtError('YAML error: {}'.format(exc.problem))
+    util.setErrorLocation(input, 1)
+    src_root_path = os.path.dirname(input) + '/'
+    dst_dir = args['deploy_dir'] + '/'
+    if 'options' in yml:
+        if 'src_dir' in yml['options']:
+            src_root_path += yml['options']['src_dir'] + '/'
+        if util.getEnv('target_platform') == 'ios' and 'ios' in yml['options']:
+            if 'dst_dir' in yml['options']['ios']:
+                dst_dir += yml['options']['ios']['dst_dir'] + '/'
+        else if util.getEnv('target_platform') == 'osx' and 'macos' in yml['options']:
+            if 'dst_dir' in yml['options']['macos']:
+                dst_dir += yml['options']['macos']['dst_dir'] + '/'
+        else if 'dst_dir' in yml['options']:
+            dst_dir += yml['options']['dst_dir']
+        del yml['options']
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    if check_dirty(src_root_path, input, out_hdr, yml):
+        copy_files(src_root_path, dst_dir, yml)
+        gen_header(out_hdr, yml)
